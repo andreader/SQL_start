5349085984a0a236d4d0cdb20bfd0f550999d246
Андрей <79538233369@ya.ru>
2024-06-30 13:15:11 +0500

    Initial

---
+++ src/main/java/pro/sky/sql_start/SqlStartApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.sql_start;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlStartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SqlStartApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/sql_start/SqlStartApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.sql_start;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlStartApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
